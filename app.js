e4344fda2dd173a97986b6c1f85ba8d81704f9a4
Your <ckiroshan@gmail.com>
2023-11-09 20:51:20 +0530

    Added the password generating function.

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ const numbers = "0123456789";
 const symbols = "~`!@#$%^&*()_-+={[}]|:;<,>.?/";
 const allChars = upperCase + lowerCase + numbers + symbols;
 
+// Password generating function
+function generatePassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (length > password.length) [(password += allChars[Math.floor(Math.random() * allChars.length)])];
+
+  passwordBox.value = password;
+}
